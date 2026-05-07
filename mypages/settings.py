2376e5a6255bec01270d9c6b4b7fd1a7fda4180f
Shalma <shalmaahalida@gmail.com>
2026-05-07 16:20:11 +0700

    fix allowed hosts railway

--- mypages/settings.py
+++ mypages/settings.py
@@ -40,7 +40,11 @@ DEBUG = not PRODUCTION
 
 HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME', '')
 
-ALLOWED_HOSTS = [f'{HEROKU_APP_NAME}.herokuapp.com']
+ALLOWED_HOSTS = [
+    'djangoweb-production-b611.up.railway.app',
+    'localhost',
+    '127.0.0.1',
+]
 
 if not PRODUCTION:
     ALLOWED_HOSTS += ['.localhost', '127.0.0.1', '[::1]']
