9b2c35f5a6ad3b72201bfdeeb245c0cdee88f956
Shalma <shalmaahalida@gmail.com>
2026-05-07 15:58:54 +0700

    fix sosmed urls view name

--- sosmed/urls.py
+++ sosmed/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = 'sosmed'
 
 urlpatterns = [
-    path('', views.list, name='list'),
+    path('', views.list_instagram, name='list'),
+    path('create/', views.create, name='create'),
+    path('update/<int:update_id>/', views.update, name='update'),
+    path('delete/<int:delete_id>/', views.delete, name='delete'),
 ]
