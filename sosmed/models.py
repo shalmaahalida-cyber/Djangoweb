475fd6196e2e96b0f7a5684e2eee418510bdf807
Shalma <shalmaahalida@gmail.com>
2026-05-07 10:17:14 +0700

    full project

---
+++ sosmed/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Instagram(models.Model):
+    nama_depan    = models.CharField(max_length=100)
+    nama_belakang = models.CharField(max_length=100)
+    username      = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.id}.{self.username}"
